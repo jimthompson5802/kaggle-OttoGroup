408783e288a804904c4f36f46a88b1740b59ad35
Jim Thompson <jimthompson5802@aol.com>
2015-04-04 10:59:36 -0400

    added global variable for data directory

--- src/CommonFunctions.R
+++ src/CommonFunctions.R
@@ -2,6 +2,10 @@
 # Common Functions
 ###
 
+# Global variables
+DATA.DIR <- "./data"
+
+# Evaulation Function
 logLossEval <- function(pred.probs, true.class) {
     # pred.probs: N-by-9 matrix of predicted probabilities
     # true.class:  true class designation
