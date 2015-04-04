f594fe205c4038725c3624b9df7428e75f65bd22
Jim Thompson <jimthompson5802@aol.com>
2015-04-04 13:54:20 -0400

    added code to make prediction

--- src/gbm_model/train_gbm.R
+++ src/gbm_model/train_gbm.R
@@ -18,15 +18,17 @@ load(paste0(DATA.DIR,"/near_zero_vars.RData"))
 
 # extract subset for inital training
 set.seed(29)
-idx <- sample(nrow(train.raw),0.10*nrow(train.raw))
+idx <- sample(nrow(train.raw),0.05*nrow(train.raw))
 train <- train.raw[idx,]
 
 # eliminate near zero Variance
 train <- train[,setdiff(names(train),c(nz.vars,"id"))]
+train$target <- factor(train$target)
 
 tr.ctrl <- trainControl(## 10-fold CV
     method = "repeatedcv",
-    number = 10)
+    number = 3,
+    classProbs=TRUE)
 
 set.seed(825)
 gbmFit1 <- train(target ~ ., data = train,
@@ -34,6 +36,13 @@ gbmFit1 <- train(target ~ ., data = train,
                  trControl = tr.ctrl,
                  ## This last option is actually one
                  ## for gbm() that passes through
-                 verbose = FALSE)
+                 verbose = FALSE,
+                 metric="Accuracy")
 gbmFit1
+
+
+# evaluate on test ste
+test <- test.raw[,setdiff(names(test.raw),c(nz.vars,"id"))]
+
+pred.probs <- predict(gbmFit1,newdata = test[,1:(ncol(test)-1)],type = "prob")
 
