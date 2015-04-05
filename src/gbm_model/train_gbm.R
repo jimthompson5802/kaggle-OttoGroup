59c2b88569ae0f5660a27fd62ac018cc983b86d2
Jim Thompson <jimthompson5802@aol.com>
2015-04-04 20:07:17 -0400

    changed model performance from caret "Accuracy" to custom Kaggle evaulation function for LogLoss

--- src/gbm_model/train_gbm.R
+++ src/gbm_model/train_gbm.R
@@ -15,10 +15,12 @@ load(paste0(DATA.DIR,"/train_calib_test.RData"))
 # get near zero Vars to eliminate
 load(paste0(DATA.DIR,"/near_zero_vars.RData"))
 
+library(doMC)
+registerDoMC(cores = 5)
 
 # extract subset for inital training
 set.seed(29)
-idx <- sample(nrow(train.raw),0.05*nrow(train.raw))
+idx <- sample(nrow(train.raw),0.1*nrow(train.raw))
 train <- train.raw[idx,]
 
 # eliminate near zero Variance
@@ -28,16 +30,23 @@ train$target <- factor(train$target)
 tr.ctrl <- trainControl(## 10-fold CV
     method = "repeatedcv",
     number = 3,
-    classProbs=TRUE)
+    classProbs=TRUE,
+    summaryFunction=caretLogLossSummary)
+
+tune.grid <-  expand.grid(interaction.depth = c(1, 3, 5),
+                        n.trees = (1:10)*50,
+                        shrinkage = 0.1)
 
 set.seed(825)
-gbmFit1 <- train(target ~ ., data = train,
+system.time(gbmFit1 <- train(target ~ ., data = train,
                  method = "gbm",
                  trControl = tr.ctrl,
                  ## This last option is actually one
                  ## for gbm() that passes through
                  verbose = FALSE,
-                 metric="Accuracy")
+                 maximize=FALSE,
+                 tuneGrid=tune.grid,
+                 metric="LogLoss"))
 gbmFit1
 
 
@@ -46,3 +55,4 @@ test <- test.raw[,setdiff(names(test.raw),c(nz.vars,"id"))]
 
 pred.probs <- predict(gbmFit1,newdata = test[,1:(ncol(test)-1)],type = "prob")
 
+logLossEval(pred.probs,test$target)
