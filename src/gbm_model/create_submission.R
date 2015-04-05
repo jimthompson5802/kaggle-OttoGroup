9438eae2342daaa901dc1bf4cf1909f342d00719
Jim Thompson <jimthompson5802@aol.com>
2015-04-04 22:52:15 -0400

    initial baseline

---
+++ src/gbm_model/create_submission.R
@@ -0,0 +1,36 @@
+###
+# generate submission for gbm model
+###
+
+library(caret)
+
+
+# import global variabels and common functions
+source("./src/CommonFunctions.R")
+WORK.DIR <- "./src/gbm_model"
+
+# get near zero Vars to eliminate
+load(paste0(DATA.DIR,"/near_zero_vars.RData"))
+
+# read kaggle submission data
+new.df <- read.csv(unz(paste0(DATA.DIR,"/test.csv.zip"),"test.csv"),stringsAsFactors=FALSE)
+
+#save id vector
+id <- new.df$id
+
+# prep the data for submission
+new.df <- new.df[,setdiff(names(new.df),c(nz.vars,"id"))]
+
+# retrive gbm model
+load(paste0(WORK.DIR,"/gbmFit1.RData"))
+
+# predict class probabilities
+pred.probs <- predict(gbmFit1,newdata = new.df,type = "prob")
+
+#create kaggle submission file
+write.csv(data.frame(id,pred.probs),file=paste0(WORK.DIR,"/submission.csv"),
+          row.names=FALSE)
+
+
+
+
