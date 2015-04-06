6aad1dba897f84c8ea390e01dfe6c8f577c5a200
Jim Thompson <jimthompson5802@aol.com>
2015-04-05 22:57:44 -0400

    initial baseline

---
+++ src/rf_model/train_model.R
@@ -0,0 +1,70 @@
+###
+# training gbm model
+###
+
+library(caret)
+library(randomForest)
+
+# Common Functions and Global variables
+source("./src/CommonFunctions.R")
+WORK.DIR <- "./src/rf_model"
+
+# get training data
+load(paste0(DATA.DIR,"/train_calib_test.RData"))
+     
+# get near zero Vars to eliminate
+load(paste0(DATA.DIR,"/near_zero_vars.RData"))
+
+library(doMC)
+registerDoMC(cores = 5)
+
+# extract subset for inital training
+set.seed(29)
+idx <- sample(nrow(train.raw),0.3*nrow(train.raw))
+train.df <- train.raw[idx,]
+
+# eliminate near zero Variance
+train.df <- train.df[,setdiff(names(train.df),c(nz.vars,"id"))]
+train.df$target <- factor(train.df$target)
+
+tr.ctrl <- trainControl(
+    method = "repeatedcv",
+    number = 10,
+    repeats=1,
+    verboseIter = TRUE,
+    classProbs=TRUE,
+    summaryFunction=caretLogLossSummary)
+
+# tune.grid <-  expand.grid(interaction.depth = 3, #c(1, 3, 5),
+#                         n.trees = (1:10)*50,
+#                         shrinkage = 0.1)
+
+set.seed(825)
+system.time(rfFit1 <- train(train.df[,1:(ncol(train.df)-1)],
+                 train.df[,ncol(train.df)],
+                 method = "rf",
+
+                 ## This last option is actually one
+                 ##  that passes through to model
+                 
+                 
+                 ## remaining train options
+                 trControl = tr.ctrl,
+#                  tuneGrid=tune.grid,
+                 maximize=FALSE,
+                 metric="LogLoss"))
+rfFit1
+
+
+# evaluate on test ste
+test <- test.raw[,setdiff(names(test.raw),c(nz.vars,"id"))]
+
+pred.probs <- predict(rfFit1,newdata = test[,1:(ncol(test)-1)],type = "prob")
+
+logLossEval(pred.probs,test$target)
+
+# save generated model
+# save(rfFit1,file=paste0(WORK.DIR,"/rfFit1.RData"))
+
+
+
