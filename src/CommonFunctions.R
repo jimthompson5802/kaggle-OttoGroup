b90744f5c24abe0ed8fe68558fd3a7eb2d6aaacc
Jim Thompson <jimthompson5802@aol.com>
2015-04-05 21:59:33 -0400

    added code to avoid numerical issues with extreme values for predicted probabilities, i.e., either 0 or 1

--- src/CommonFunctions.R
+++ src/CommonFunctions.R
@@ -15,16 +15,19 @@ logLossEval <- function(pred.probs, true.class) {
     y <- do.call(rbind,ll)
     colnames(y) <- paste0("Class_",1:9)
     
+    # adjust probabilities to avoid numerical issues with log() function
+    new.probs <- apply(pred.probs,c(1,2),function(elem){max(min(elem,(1-1e-15)),1e-15)})
+    
     # get row totals
-    row.totals <- apply(pred.probs,1,sum)
+    row.totals <- apply(new.probs,1,sum)
     
-    ans <- -sum(log(pred.probs/row.totals)*y)/nrow(pred.probs)
+    ans <- -sum(log(new.probs/row.totals)*y)/nrow(new.probs)
     
-    if (is.na(ans)) {
-        stop("Returning NA from LogLoss Function.")
-    } else {
-        return(ans) 
+    if (is.nan(ans) | is.na(ans)) {
+        stop("Returning NAN or NA from LogLoss Function.")
     }
+        
+    return(ans) 
     
     
 }
@@ -32,6 +35,9 @@ logLossEval <- function(pred.probs, true.class) {
 # caret custom model performance function for log-loss
 caretLogLossSummary <- function(data,lev,model) {
     out <- logLossEval(data[,3:ncol(data)],data[,"obs"])
+    if (is.na(out) | is.nan(out)) {
+        stop("Error in LogLoss Function")
+    }
     names(out) <- "LogLoss"
     out
 }
--- src/TestBed.R
+++ src/TestBed.R
@@ -11,5 +11,7 @@ source("./src/CommonFunctions.R")
 test.probs <- matrix(rep(1/9,nrow(test.raw)*9),ncol=9)
 colnames(test.probs) <- paste0("Class_",1:9)
 
+test.probs[1,1] <- 0
+
 
 logLossEval(test.probs,test.raw$target)
