a59403a684cb2319c9dee0adff8ca6ed70016b43
Jim Thompson <jimthompson5802@aol.com>
2015-04-09 21:45:34 -0400

    date.time        "2015-04-09 21:25:47" model            "rf" user.cpu.time    "1187.613" sys.cpu.time     "2.907" elapsed.time     "1190.599" num.observations "29704" num.features     "93" score            "0.5812919" mtry             "47" ntree            "2000"

--- src/rf_model/create_submission.R
+++ src/rf_model/create_submission.R
@@ -1,13 +1,14 @@
 ###
-# generate submission for gbm model
+# generate submission for rf model
 ###
 
 library(caret)
+library(randomForest)
 
 
 # import global variabels and common functions
 source("./src/CommonFunctions.R")
-WORK.DIR <- "./src/gbm_model"
+WORK.DIR <- "./src/rf_model"
 
 # get near zero Vars to eliminate
 load(paste0(DATA.DIR,"/near_zero_vars.RData"))
@@ -19,13 +20,13 @@ new.df <- read.csv(unz(paste0(DATA.DIR,"/test.csv.zip"),"test.csv"),stringsAsFac
 id <- new.df$id
 
 # prep the data for submission
-new.df <- new.df[,setdiff(names(new.df),c(nz.vars,"id"))]
+new.df <- new.df[,setdiff(names(new.df),c("id"))]
 
-# retrive gbm model
-load(paste0(WORK.DIR,"/gbmFit1.RData"))
+# retrive rf model
+load(paste0(WORK.DIR,"/rfFit1_2015-04-09_21_25_47.RData"))
 
 # predict class probabilities
-pred.probs <- predict(gbmFit1,newdata = new.df,type = "prob")
+pred.probs <- predict(rfFit1,newdata = new.df,type = "prob")
 
 #create kaggle submission file
 write.csv(data.frame(id,pred.probs),file=paste0(WORK.DIR,"/submission.csv"),
