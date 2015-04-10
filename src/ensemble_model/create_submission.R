909d8eb3430890a3746b3e5b7e116c66bacd6645
Jim Thompson <jimthompson5802@aol.com>
2015-04-09 22:38:05 -0400

    initial baseline with rf and gbm models

---
+++ src/ensemble_model/create_submission.R
@@ -0,0 +1,72 @@
+###
+#  create ensemble model combining selected models
+###
+
+
+library(caret)
+library(gbm)
+library(randomForest)
+
+
+# import global variabels and common functions
+source("./src/CommonFunctions.R")
+WORK.DIR <- "./src/ensemble_model"
+
+# get near zero Vars to eliminate
+load(paste0(DATA.DIR,"/near_zero_vars.RData"))
+
+# read kaggle submission data
+new.df <- read.csv(unz(paste0(DATA.DIR,"/test.csv.zip"),"test.csv"),stringsAsFactors=FALSE)
+
+#save id vector
+id <- new.df$id
+
+#
+# make gbm prediction
+#
+
+# prep the data for submission
+new.df <- new.df[,setdiff(names(new.df),c(nz.vars,"id"))]
+
+# retrive gbm model
+load("./src/gbm_model/gbmFit1_2015-04-07_21_12_42.RData")
+
+# predict class probabilities
+gbm.probs <- predict(gbmFit1,newdata = new.df,type = "prob")
+
+# combine with id
+gbm.probs <- data.frame(id,gbm.probs)
+
+#
+# make rf prediction
+#
+
+# read kaggle submission data
+new.df <- read.csv(unz(paste0(DATA.DIR,"/test.csv.zip"),"test.csv"),stringsAsFactors=FALSE)
+
+#save id vector
+id <- new.df$id
+
+# prep the data for submission
+new.df <- new.df[,setdiff(names(new.df),c("id"))]
+
+# retrive rf model
+load("./src/rf_model/rfFit1_2015-04-09_21_25_47.RData")
+
+# predict class probabilities
+rf.probs <- predict(rfFit1,newdata = new.df,type = "prob")
+
+# recombine with id
+rf.probs <- data.frame(id,rf.probs)
+
+#
+# Average the individual probablities
+#
+
+pred.probs <- (rf.probs[,2:10] + gbm.probs[,2:10])/2
+
+
+#create kaggle submission file
+write.csv(data.frame(id,pred.probs),file=paste0(WORK.DIR,"/submission.csv"),
+          row.names=FALSE)
+
