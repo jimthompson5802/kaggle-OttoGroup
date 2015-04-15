059e645d7ce8097a2ca8f06fc13302bd65fcb386
Jim Thompson <jimthompson5802@aol.com>
2015-04-14 22:48:51 -0400

    initial set up for creating one vs all submission using gbm as the base model

--- src/gbm2_model/create_submission.R
+++ src/gbm2_model/create_submission.R
@@ -1,5 +1,5 @@
 ###
-# generate submission for gbm model
+# generate submission for one vs all model using gbm
 ###
 
 library(caret)
@@ -7,7 +7,7 @@ library(caret)
 
 # import global variabels and common functions
 source("./src/CommonFunctions.R")
-WORK.DIR <- "./src/gbm_model"
+WORK.DIR <- "./src/gbm2_model"
 
 # get near zero Vars to eliminate
 load(paste0(DATA.DIR,"/near_zero_vars.RData"))
@@ -19,13 +19,16 @@ new.df <- read.csv(unz(paste0(DATA.DIR,"/test.csv.zip"),"test.csv"),stringsAsFac
 id <- new.df$id
 
 # prep the data for submission
-new.df <- new.df[,setdiff(names(new.df),c(nz.vars,"id"))]
+new.df <- new.df[,setdiff(names(new.df),c("id"))]
 
-# retrive gbm model
-load(paste0(WORK.DIR,"/gbmFit1_2015-04-07_21_12_42.RData"))
+# retrive one versus all gbm model
+load(paste0(WORK.DIR,"/gbm.mdls_2015-04-14_22_32_15.RData"))
 
 # predict class probabilities
-pred.probs <- predict(gbmFit1,newdata = new.df,type = "prob")
+ll <- lapply(classes,predictForOneClass,gbm.mdls,new.df)
+names(ll) <- classes
+
+pred.probs <- do.call(cbind,ll)
 
 #create kaggle submission file
 write.csv(data.frame(id,pred.probs),file=paste0(WORK.DIR,"/submission.csv"),
