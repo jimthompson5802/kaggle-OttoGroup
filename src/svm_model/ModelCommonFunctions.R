b0583b262b9f59731c6886e3f667f7810987eb3b
Jim Thompson <jimthompson5802@aol.com>
2015-04-15 22:14:00 -0400

    update submission to use refactored data prep and model conventions

--- src/svm_model/ModelCommonFunctions.R
+++ src/svm_model/ModelCommonFunctions.R
@@ -2,17 +2,26 @@
 #  Model specific common functions
 ###
 
-prepModelData <- function(df){
+prepModelData <- function(df,only.predictors=FALSE){
     # df: raw data
     # returns a list(predictors,response)
     
     # get near zero Vars to eliminate
     load(paste0(DATA.DIR,"/near_zero_vars.RData"))
     
-    # eliminate unwanted variables
-    predictors <- df[,setdiff(names(df),c(nz.vars,"id","target"))]
-    response <- factor(df$target)
+   
     
-    return(list(predictors=predictors,response=response))
+    if (only.predictors) {
+        # eliminate unwanted variables
+        predictors <- df[,setdiff(names(df),c(nz.vars,"id"))]
+        return(list(predictors=predictors))
+    } else {
+        # eliminate unwanted variables
+        predictors <- df[,setdiff(names(df),c(nz.vars,"id","target"))]
+        response <- factor(df$target)
+        
+        return(list(predictors=predictors,response=response))
+    }
+
 }
 
