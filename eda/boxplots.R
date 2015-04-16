dcc585221e351995fb234f6971e033dd5639a66a
Jim Thompson <jimthompson5802@aol.com>
2015-04-16 17:29:20 -0400

    testing out some views

--- eda/boxplots.R
+++ eda/boxplots.R
@@ -10,9 +10,9 @@ data.dir <- "./data"
 
 load(file=paste0(data.dir,"/train_calib_test.RData"))
 
-boxplot(train.raw$feat_62~train.raw$target)
+boxplot(train.raw$feat_1~train.raw$target)
 
-qplot(target,feat_62,data=train.raw,geom="boxplot")
+qplot(target,feat_1,data=train.raw,geom="boxplot")
 
 by(train.raw[,2:(ncol(train.raw)-1)],train.raw$target,summary)
 
--- eda/initial_eda.R
+++ eda/initial_eda.R
@@ -3,6 +3,7 @@
 ###
 
 library(Hmisc)
+library(plyr)
 
 data.dir <- "./data"
 
@@ -14,9 +15,10 @@ summary(train.raw)
 
 # determine number of distict values
 distinct.values <- apply(train.raw[,2:ncol(train.raw)],2,function(x){length(table(x))})
+distinct.value.list <- apply(train.raw[,2:ncol(train.raw)],2,function(x){table(x)})
 
 # chi-sq test
-bin.df <- data.frame(lapply(train.raw[,2:(ncol(train.raw)-1)],function(x){cut2(x,quantile(x,seq(0,1,0.25)))}))
-r <- apply(train.raw[,2:(ncol(train.raw)-1)],2,function(x){chisq.test(x,train.raw$target)})
+# bin.df <- data.frame(lapply(train.raw[,2:(ncol(train.raw)-1)],function(x){cut2(x,quantile(x,seq(0,1,0.25)))}))
+r <- apply(train.raw[,2:(ncol(train.raw)-1)],2,function(x){chisq.test(x,train.raw$target,simulate.p.value=TRUE)})
 
 sapply(r,function(x){x$p.value})
