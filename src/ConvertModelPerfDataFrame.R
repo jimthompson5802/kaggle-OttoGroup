af74b6adbf283e717a04d6b5186f3aff03c6be8a
Jim Thompson <jimthompson5802@aol.com>
2015-04-15 23:36:25 -0400

    converted one vs all gbm model performance data frame to expanded format

--- src/ConvertModelPerfDataFrame.R
+++ src/ConvertModelPerfDataFrame.R
@@ -2,16 +2,16 @@
 # code to convert to new model Performance data frame
 #
 
-WORK.DIR <- "./src/rf_model"
+WORK.DIR <- "./src/gbm2_model"
 
 # convert current bestTune data to string
-bestTune <- apply(modPerf.df["mtry"],1,function(x){as.list(x)})
+bestTune <- apply(modPerf.df[,9:35],1,function(x){as.list(x)})
 bestTune <- sapply(bestTune,function(x){paste(names(x),as.character(x),sep="=",collapse=",")},
                    simplify=TRUE)
 
-model.parms <- apply(modPerf.df["ntree"],1,function(x){as.list(x)})
-model.parms <- sapply(model.parms,function(x){paste(names(x),as.character(x),sep="=",collapse=",")},
-                   simplify=TRUE)
+# model.parms <- apply(modPerf.df["ntree"],1,function(x){as.list(x)})
+# model.parms <- sapply(model.parms,function(x){paste(names(x),as.character(x),sep="=",collapse=",")},
+#                    simplify=TRUE)
 
 
 n <- length(bestTune)
@@ -19,7 +19,7 @@ modelPerf.df <- data.frame(modPerf.df[,1:8],
                            improved=rep("",n),
                            bestTune=bestTune,
                            tune.grid=rep("",n),
-                           model.parms=model.parms,
+                           model.parms=rep("",n),
                            comment=rep("",n),
                            features=modPerf.df[,"features"],
                            stringsAsFactors=FALSE)
