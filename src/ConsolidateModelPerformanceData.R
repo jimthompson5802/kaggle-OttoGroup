64f5f501d4cd9e83d119f24988f9a4689f713ee2
Jim Thompson <jimthompson5802@aol.com>
2015-04-19 10:31:58 -0400

    initial baseline

---
+++ src/ConsolidateModelPerformanceData.R
@@ -0,0 +1,21 @@
+###
+# Consolidate model performance data
+###
+
+source("./src/CommonFunctions.R")
+
+
+model.dirs <- list.dirs("./src",recursive=FALSE)
+model.dirs <- setdiff(model.dirs,c("skeleton_model","./src/ada_model",
+                                   "./src/ensemble_model",
+                                   "./src/c50_model"))
+
+#initialize consolidation data frame
+consolidatedModelPerf.df <- data.frame()
+
+for (dir in model.dirs){
+    load(paste(dir,"modelPerf.RData",sep="/"))
+    df <- data.frame(dir=rep(dir,nrow(modelPerf.df)),modelPerf.df,stringsAsFactors=FALSE)
+    consolidatedModelPerf.df <- rbind(consolidatedModelPerf.df,df)
+}
+
