139436495a4bb0344888e3eb5b5c59dbe1eb0d98
Jim Thompson <jimthompson5802@aol.com>
2015-04-28 06:02:28 -0400

    added corrplot and code to extract high correlated pairs

--- eda/nzv_correlations.R
+++ eda/nzv_correlations.R
@@ -3,6 +3,8 @@
 ###
 
 library(caret)
+library(corrplot)
+library(tidyr)
 
 data.dir <- "./data"
 
@@ -20,9 +22,21 @@ save(nz.vars,file=paste0(data.dir,"/near_zero_vars.RData"))
 
 correlations <- cor(predictors)
 
+png("./eda/corrplot.png",height=17, width=11, units="in", res=72)
+corrplot(correlations, order="hclust")
+dev.off()
+
 coridx <- findCorrelation(correlations,cutoff=0.8)
 
 corr.vars <- names(predictors)[coridx]
 
 save(corr.vars, file=paste0(data.dir,"/correlated_vars.RData"))
+
+#flatten the correlation matrrix and extract features with high correlation
+df <- data.frame(feature.x=row.names(correlations),correlations,stringsAsFactors=FALSE)
+
+df <- gather(df,feature.y,correlation,contains("feat_"))
+
+subset(df,abs(correlation) >= 0.7 & abs(correlation) < 1.0)
+
 
