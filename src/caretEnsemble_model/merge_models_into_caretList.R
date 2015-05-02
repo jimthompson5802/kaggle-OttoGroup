0dc157f3c517ee4f45077239d8439eab4a5eed51
Jim Thompson <jimthompson5802@aol.com>
2015-05-01 21:57:42 -0400

    initial check-in

--- src/caretEnsemble_model/merge_models_into_caretList.R
+++ src/caretEnsemble_model/merge_models_into_caretList.R
@@ -2,5 +2,19 @@
 # merge models into caretList structure
 ###
 
+library(caretEnsemble)
 
-for (cls in PRODUCT.CLASSES)
+all.classes.test <- list()
+
+for (cls in PRODUCT.CLASSES) {
+    for (i in 1:length(other.mdls)) {
+        # add models to respective class
+        caret.list[[cls]][[other.mdls[[i]]$model.parm$method]] <- other.mdls[[i]]$models[[cls]]
+
+    }
+    all.classes.test[[cls]] <- caretEnsemble(caret.list[[cls]])
+}
+
+# accumulate the time for training
+Reduce("+",lapply(other.mdls,function(x){x$time.data}))
+
