b770ebd2daf70f119f33b8c47499c6cb293015a6
Jim Thompson <jimthompson5802@aol.com>
2015-05-09 15:48:52 -0400

    added synthetic features and increased plot resolution

--- eda/feature_boxplot.R
+++ eda/feature_boxplot.R
@@ -12,8 +12,9 @@ load(paste0(DATA.DIR,"/train_calib_test.RData"))
 predictor.vars <- setdiff(names(train.raw), c("id","target"))
 
 train.raw$non.zero.count <- apply(train.raw[,predictor.vars], 1, function(row){sum(row>0)})
+train.raw$count.total <- apply(train.raw[,predictor.vars],1,sum)
 
-predictor.vars <- c(predictor.vars,"non.zero.count")
+predictor.vars <- c(predictor.vars,"non.zero.count","count.total")
 
 
 # convert to long format 
@@ -22,7 +23,7 @@ df <- gather(train.raw, feature, value, one_of(predictor.vars))
 df$feature.id <- do.call(rbind,strsplit(as.character(df$feature),"_"))[,2]
 df$class.id <- do.call(rbind,strsplit(df$target,"_"))[,2]
 
-png("./eda/feature_boxplot.png",height=17, width=11, units="in",res=100)
+png("./eda/feature_boxplot.png",height=17, width=11, units="in",res=300)
 qplot(class.id,value,data=df,geom="boxplot") + 
     facet_wrap(~feature,ncol=8,scales="free_y") +
     xlab("Class")
--- eda/feature_density.R
+++ eda/feature_density.R
@@ -16,13 +16,18 @@ rm(calib.raw,test.raw)
 
 predictor.vars <- setdiff(names(train.raw), c("id","target"))
 
+train.raw$non.zero.count <- apply(train.raw[,predictor.vars], 1, function(row){sum(row>0)})
+train.raw$count.total <- apply(train.raw[,predictor.vars],1,sum)
+
+predictor.vars <- c(predictor.vars,"non.zero.count","count.total")
+
 # convert to long format 
-df <- gather(train.raw, feature, value, feat_1:feat_93)
+df <- gather(train.raw, feature, value, one_of(predictor.vars))
 
 df$feature.id <- do.call(rbind,strsplit(as.character(df$feature),"_"))[,2]
 df$class.id <- do.call(rbind,strsplit(df$target,"_"))[,2]
 
-png("./eda/feature_density.png",height=17, width=11, units="in",res=100)
+png("./eda/feature_density.png",height=17, width=11, units="in",res=300)
 ggplot(df) + 
     geom_density(aes(x=value, colour=target)) +
     facet_wrap(~feature,ncol=8,scales="free") 
