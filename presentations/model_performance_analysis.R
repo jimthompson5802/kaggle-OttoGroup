f6f33840fd81e325bd847fe618fab3295e5f2791
Jim Thompson <jimthompson5802@aol.com>
2015-06-03 22:08:02 -0400

    added code to create model performance slide

--- presentations/model_performance_analysis.R
+++ presentations/model_performance_analysis.R
@@ -41,3 +41,38 @@ p1 <- ggplot(summary.df) +
 png("./presentations/model_runs_analysis.png",width=8,height=6, units="in",res=300)
 print(p1)
 dev.off()
+
+
+# create list of models and their best performance
+# eliminate the invalid scores
+df3 <- subset(modPerf.df, score > 0.45 & model!="caretEnsemble")
+
+# sort by model and score
+df3 <- df3[order(df3$dir,df3$score),]
+
+# get the three best scores for each model
+df3 <- ddply(df3,.(dir),head,3)
+
+# parse out the model identifier
+ll <- strsplit(df3$dir,split="/")
+df3$model.id <- sapply(ll,function(x){x[[3]]},simplify = TRUE)
+ll <- strsplit(df3$model.id,split="_")
+df3$model.id <- sapply(ll,function(x){x[[1]]},simplify=TRUE)
+
+df3 <- subset(df3,select=c("model.id","num.observations","num.features","score"))
+df3 <- df3[order(df3$score),]
+df3$model.id <- paste(df3$model.id,1:nrow(df3),sep="_")
+
+p2 <- ggplot(df3) +
+    geom_bar(aes(x=reorder(model.id,score),y=score), stat="identity",
+             fill="blue") + 
+    scale_y_continuous(limits=c(0,2), breaks = seq(0, 2, 0.2)) +
+    xlab("Model") +
+    ylab("(Better) MLL Score (Worse)") + 
+    ggtitle("Model Performance on test.raw Data Set") +
+    theme(axis.text.x=element_text(angle=90))
+
+png("./presentations/model_score_analysis.png",width=8,height=6, units="in",res=300)
+print(p2)
+dev.off()
+
