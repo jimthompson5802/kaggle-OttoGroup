5c7f739ba72f855b36ecc1dce7c5d02def3aac7f
Jim Thompson <jimthompson5802@aol.com>
2015-06-06 20:00:19 -0400

    Allowed for recursive search for model performance data

--- src/ConsolidateModelPerformanceData.R
+++ src/ConsolidateModelPerformanceData.R
@@ -5,7 +5,7 @@
 source("./src/CommonFunctions.R")
 
 
-model.dirs <- list.dirs("./src",recursive=FALSE)
+model.dirs <- list.dirs("./src",recursive=TRUE)
 model.dirs <- setdiff(model.dirs,c("skeleton_model","./src/ada_model",
                                    "./src/ensemble_model", "./src/py_model",
                                    "./src/c50_model","./src/sandbox"))
@@ -14,9 +14,12 @@ model.dirs <- setdiff(model.dirs,c("skeleton_model","./src/ada_model",
 consolidatedModelPerf.df <- data.frame()
 
 for (dir in model.dirs){
-    load(paste(dir,"modelPerf.RData",sep="/"))
-    df <- data.frame(dir=rep(dir,nrow(modelPerf.df)),modelPerf.df,stringsAsFactors=FALSE)
-    consolidatedModelPerf.df <- rbind(consolidatedModelPerf.df,df)
+    full.file.name <- paste(dir,"modelPerf.RData",sep="/")
+    if (file.exists(full.file.name)) {
+        load(paste(dir,"modelPerf.RData",sep="/"))
+        df <- data.frame(dir=rep(dir,nrow(modelPerf.df)),modelPerf.df,stringsAsFactors=FALSE)
+        consolidatedModelPerf.df <- rbind(consolidatedModelPerf.df,df)
+    }
 }
 
 write.table(consolidatedModelPerf.df,
